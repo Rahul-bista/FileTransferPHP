43629a30f96c8044501bd146bbaa1ac7460433e8
Tej Magar <tejmagar20510@gmail.com>
2024-06-25 13:52:57 +0545

    FIX: auto create database and tables

--- db.php
+++ db.php
@@ -4,29 +4,32 @@ $db = "FileTransfer";
 $user = "root";
 $pass = "";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
     die("error while connecting to database, error: " . $conn->connect_error);
 }
 
-// $sql = "CREATE TABLE rooms (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-//     room_code VARCHAR(255) UNIQUE NOT NULL,
-//     owner_token VARCHAR(255) NOT NULL,
-//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-// )";
+$conn->query("CREATE DATABASE IF NOT EXISTS $db");
+$conn = new mysqli($host, $user, $pass, $db);
 
-// $conn->query($sql);
+$sql = "CREATE TABLE IF NOT EXISTS rooms (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_code VARCHAR(255) UNIQUE NOT NULL,
+    owner_token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
 
-// $sql = "CREATE TABLE files (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-//     room_id INT NOT NULL,
-//     file_name VARCHAR(255) NOT NULL,
-//     file_path VARCHAR(255) NOT NULL,
-//     uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//     FOREIGN KEY (room_id) REFERENCES rooms(id)
-// )";
+$conn->query($sql);
 
-// $conn->query($sql);
+$sql = "CREATE TABLE IF NOT EXISTS files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_id INT NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+)";
+
+$conn->query($sql);
 ?>
